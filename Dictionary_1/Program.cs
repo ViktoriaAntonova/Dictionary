562c5530ad0f385d6c294cc531aebe045e55e900
MEGA <MEGA@gmail.com>
2025-03-25 17:41:35 +0200

    Add project files.

---
+++ Dictionary_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,string> dic = new Dictionary<int,string>();
+            dic.Add(101, "Ivan");
+            dic.Add(103, "Viktoria");
+            dic.Add(105, "Marin");
+            dic.Add(107, "Iliana");
+            int a =int.Parse(Console.ReadLine());
+            if(dic.ContainsKey(a))
+            {
+                Console.WriteLine("Name:"+dic[a]);
+            }
+            else
+            {
+                Console.WriteLine("Ne suchtestvuva");
+            }
+        }
+    }
+}

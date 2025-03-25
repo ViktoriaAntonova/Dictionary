c047d587f5d60a7f05ccce0d7da3726639c9fc68
MEGA <MEGA@gmail.com>
2025-03-25 17:55:01 +0200

    ZAD3

---
+++ Dictionary_3/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary_3
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Employee(int id, string name)
+        {
+            Id = id; Name = name;
+        }
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {Name}. ";
+        }
+    }
+}
---
+++ Dictionary_3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<int, Employee> employees = new SortedDictionary<int, Employee>();
+            string command = Console.ReadLine();
+            Console.WriteLine("Vuvedete danni ili break za prekusvane:");
+            while (command.ToLower() != "break")
+            {
+                Console.Write("ID: ");
+                int id;
+                if (int.TryParse(Console.ReadLine(), out id))
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Employee employee = new Employee(id, name);
+                    if (!employees.ContainsKey(id)) 
+                    {
+                        employees.Add(id, employee);
+                    }
+                    else
+                    {
+                        Console.WriteLine("ID sushtestvuva");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine("Slujiteli");
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee.Value); 
+            }
+        }
+    }
+}
